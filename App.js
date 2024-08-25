46a6f16db7cf2f095a96903d5c54f8f3545a292c
Anushkabajaj147 <anushkabajaj41@gmail.com>
2024-08-25 21:05:28 +0530

    Quote app

---
+++ App.js
@@ -0,0 +1,74 @@
+import { View, Text, TouchableOpacity,StatusBar,Linking } from 'react-native';
+import React, { useState,useEffect } from 'react';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Tts from 'react-native-tts';
+import Clipboard from '@react-native-clipboard/clipboard';
+import Snackbar from 'react-native-snackbar';
+
+Tts.setDefaultLanguage('en-GB');
+Tts.setDefaultVoice('com.apple.ttsbundle.Moira-compact');
+Tts.setDefaultRate(0.5);
+Tts.setDefaultPitch(0.9);
+
+const App = () => {
+  const[Quote,setQuote]=useState('Loading...');
+  const[Author,setAuthor]=useState('Loading...');
+  const[isLoading,setIsLoading]=useState(false);
+  const[isSpeaking,setIsSpeaking]=useState(false);
+
+  const randomQuote=()=>{
+    setIsLoading(true);
+     fetch("https://api.quotable.io/random").then(res=>res.json()).then(result=>{/*console.log(result.content)*/;
+     setQuote(result.content);
+     setAuthor(result.author); 
+     setIsLoading(false);
+    });
+  }
+  useEffect(()=>{
+    randomQuote();
+  },[]);
+
+ const speakNow=()=>{
+    Tts.stop();
+    Tts.speak(Quote+'by'+Author);
+    Tts.addEventListener('tts-start',(event)=>setIsSpeaking(true));
+    Tts.addEventListener('tts-finish',(event)=>setIsSpeaking(false));
+  }
+  const copyToClipboard=()=>{
+   Clipboard.setString(Quote);
+    Snackbar.show({
+      text: 'Quote copied!',
+      duration: Snackbar.LENGTH_SHORT,
+    });
+  }
+  const tweetNow=()=>{
+    const url="https://twitter.com/intent/tweet?text="+Quote;
+    Linking.openURL(url);
+  }
+  return (
+    <View style={{flex:1,justifyContent:"center",alignItems:"center",backgroundColor:"#f08080"}}>  
+     <StatusBar barStyle="light-content"/>
+    <View style={{width:'90%',backgroundColor:"#fff",borderRadius:20,padding:20}}>  
+      <Text style={{textAlign:"center",fontSize:26,fontWeight:"600",color:"#333" ,marginBottom:20,}}>Quote Of The Day </Text>
+      <FontAwesome5 name="quote-left" style={{fontSize:20,marginBottom:-12}} color="#000"/>
+       <Text style={{color:"#000",fontSize:16,lineHeight:26,letterSpacing:1.1,fontWeight:'400',textAlign:'center',marginBottom:10,paddingHorizontal:30,}}> {Quote}</Text>
+       <FontAwesome5 name="quote-right" style={{fontSize:20,textAlign:'right',marginTop:-20,marginBottom:20}} color="#000"/>
+       <Text style={{textAlign:'right',fontWeight:"300",fontStyle:"italic",fontSize:16,color:'#000'}}>--{Author}</Text>
+      <TouchableOpacity onPress={randomQuote} style={{backgroundColor: isLoading?"#f08080":"#f08080",padding:20,borderRadius:30,marginVertical:20,}}>
+        <Text style={{color:"#fff",fontSize:18,textAlign:"center"}}>{isLoading?"Loading...":"New Quote"}</Text>
+      </TouchableOpacity>
+      <View style={{flexDirection:'row',justifyContent:'space-around'}}>
+        <TouchableOpacity onPress={speakNow} style={{borderWidth:2,borderColor:"#f08080",borderRadius:50,padding:15,backgroundColor:isSpeaking?'#f08080':'#fff' }}><FontAwesome name="volume-up" size={22} color={ isSpeaking?'#fff' :'#f08080'}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={copyToClipboard} style={{borderWidth:2,borderColor:"#f08080",borderRadius:50,padding:15, }}><FontAwesome5 name="copy" size={22} color="#f08080"/>
+       </TouchableOpacity>
+       <TouchableOpacity onPress={tweetNow} style={{borderWidth:2,borderColor:"#f08080",borderRadius:50,padding:15, }}><FontAwesome name="twitter" size={22} color="#f08080"/>
+       </TouchableOpacity>
+      </View>
+      </View>
+    </View>
+  );
+};
+
+export default App;
